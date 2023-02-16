2cfca4b9bb6c8550dfd903346cf5e5d511d28488
Johan Seligman <89506001+1917726@users.noreply.github.com>
2023-02-15 19:47:09 -0600

    Create models.js

---
+++ backend/models/models.js
@@ -0,0 +1,123 @@
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//collection for clientData
+let clientDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    firstName: {
+        type: String,
+        require: true
+    },
+    middleName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    phoneNumbers: {
+        type: Array,
+        required: true,
+        unique: true
+    },
+    address: {
+        line1: {
+            type: String
+        },
+        line2: {
+            type: String,
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: String,
+        }
+    },
+    organization:[{
+        type: String
+    }]
+}, {
+    collection: 'clientData',
+    timestamps: true
+});
+
+//collection for eventData
+let eventDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    eventName: {
+        type: String,
+        require: true
+    },
+    services: {
+        type: Array
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    address: {
+        line1: {
+            type: String
+        },
+        line2: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: String,
+        }
+    },
+    description: {
+        type: String,
+    },
+    attendees: [{
+        type: String
+    }],
+    organization:[{
+        type: String
+    }]
+}, {
+    collection: 'eventData'
+});
+
+//collection for organData
+let organDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    orgName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    orgEmail: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    orgPhone:{
+        type: String,
+    }
+}, {
+    collection: 'organData'
+});
+
+// create models from mongoose schemas
+const clientdata = mongoose.model('clientData', clientDataSchema);
+const eventdata = mongoose.model('eventData', eventDataSchema);
+const organdata = mongoose.model('organData', organDataSchema);
+
+// package the models in an object to export 
+module.exports = { clientdata, eventdata, organdata }
